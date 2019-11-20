25218e2fa411c85174dc855cdc53e1597bb653b9
Roland Rabien <figbug@gmail.com>
2019-11-19 19:24:14 -0800

    Lots of s

--- Scripts/pcbuildprojects.bat
+++ Scripts/pcbuildprojects.bat
@@ -50,22 +50,22 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 copy "%ROOT%\plugins\slOscilloscope\Builds\VisualStudio2017\x64\Release64\VST\Oscilloscope_64b.dll" bin
-copy "%ROOT%\plugin\Spectrum Analyzer\Builds\VisualStudio2017\x64\Release64\VST\SpectrumAnalyzer_64b.dll" bin
-copy "%ROOT%\plugin\Tone Generator\Builds\VisualStudio2017\x64\Release64\VST\ToneGenerator_64b.dll" bin
-copy "%ROOT%\plugin\Channel Mute\Builds\VisualStudio2017\x64\Release64\VST\ChannelMute_64b.dll" bin
-copy "%ROOT%\plugin\Maths\Builds\VisualStudio2017\x64\Release64\VST\Maths_64b.dll" bin
-copy "%ROOT%\plugin\AB Tester\Builds\VisualStudio2017\x64\Release64\VST\ABTester_64b.dll" bin
-copy "%ROOT%\plugin\AddInvert\Builds\VisualStudio2017\x64\Release64\VST\AddInvert_64b.dll" bin
-copy "%ROOT%\plugin\SFX8\Builds\VisualStudio2017\x64\Release64\VST\SFX8_64b.dll" bin
+copy "%ROOT%\plugins\Spectrum Analyzer\Builds\VisualStudio2017\x64\Release64\VST\SpectrumAnalyzer_64b.dll" bin
+copy "%ROOT%\plugins\Tone Generator\Builds\VisualStudio2017\x64\Release64\VST\ToneGenerator_64b.dll" bin
+copy "%ROOT%\plugins\Channel Mute\Builds\VisualStudio2017\x64\Release64\VST\ChannelMute_64b.dll" bin
+copy "%ROOT%\plugins\Maths\Builds\VisualStudio2017\x64\Release64\VST\Maths_64b.dll" bin
+copy "%ROOT%\plugins\AB Tester\Builds\VisualStudio2017\x64\Release64\VST\ABTester_64b.dll" bin
+copy "%ROOT%\plugins\AddInvert\Builds\VisualStudio2017\x64\Release64\VST\AddInvert_64b.dll" bin
+copy "%ROOT%\plugins\SFX8\Builds\VisualStudio2017\x64\Release64\VST\SFX8_64b.dll" bin
 
 copy "%ROOT%\plugins\slOscilloscope\Builds\VisualStudio2017\Win32\Release\VST\Oscilloscope_32b.dll" bin
-copy "%ROOT%\plugin\Spectrum Analyzer\Builds\VisualStudio2017\Win32\Release\VST\SpectrumAnalyzer_32b.dll" bin
-copy "%ROOT%\plugin\Tone Generator\Builds\VisualStudio2017\Win32\Release\VST\ToneGenerator_32b.dll" bin
-copy "%ROOT%\plugin\Channel Mute\Builds\VisualStudio2017\Win32\Release\VST\ChannelMute_32b.dll" bin
-copy "%ROOT%\plugin\Maths\Builds\VisualStudio2017\Win32\Release\VST\Maths_32b.dll" bin
-copy "%ROOT%\plugin\AB Tester\Builds\VisualStudio2017\Win32\Release\VST\ABTester_32b.dll" bin
-copy "%ROOT%\plugin\AddInvert\Builds\VisualStudio2017\Win32\Release\VST\AddInvert_32b.dll" bin
-copy "%ROOT%\plugin\SFX8\Builds\VisualStudio2017\Win32\Release\VST\SFX8_32b.dll" bin
+copy "%ROOT%\plugins\Spectrum Analyzer\Builds\VisualStudio2017\Win32\Release\VST\SpectrumAnalyzer_32b.dll" bin
+copy "%ROOT%\plugins\Tone Generator\Builds\VisualStudio2017\Win32\Release\VST\ToneGenerator_32b.dll" bin
+copy "%ROOT%\plugins\Channel Mute\Builds\VisualStudio2017\Win32\Release\VST\ChannelMute_32b.dll" bin
+copy "%ROOT%\plugins\Maths\Builds\VisualStudio2017\Win32\Release\VST\Maths_32b.dll" bin
+copy "%ROOT%\plugins\AB Tester\Builds\VisualStudio2017\Win32\Release\VST\ABTester_32b.dll" bin
+copy "%ROOT%\plugins\AddInvert\Builds\VisualStudio2017\Win32\Release\VST\AddInvert_32b.dll" bin
+copy "%ROOT%\plugins\SFX8\Builds\VisualStudio2017\Win32\Release\VST\SFX8_32b.dll" bin
 
 cd bin
 7z a PAPU_Win.zip PAPU_32b.dll PAPU_64b.dll
