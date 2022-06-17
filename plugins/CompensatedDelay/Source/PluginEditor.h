85a753472373268d9cb3ac991fd70f168637009a
Roland Rabien <figbug@gmail.com>
2022-06-16 21:17:29 -0700

    Build errors

--- plugins/CompensatedDelay/Source/PluginEditor.h
+++ plugins/CompensatedDelay/Source/PluginEditor.h
@@ -17,7 +17,7 @@
 /**
 */
 class CompensatedDelayAudioProcessorEditor : public gin::ProcessorEditor,
-                                             private gin::Parameter::Listener
+                                             private gin::Parameter::ParameterListener
 {
 public:
     CompensatedDelayAudioProcessorEditor (CompensatedDelayAudioProcessor&);
@@ -27,7 +27,7 @@ public:
     void resized() override;
 
 private:
-    void parameterChanged (gin::Parameter* param) override;
+    void valueUpdated (gin::Parameter* param) override;
     
     CompensatedDelayAudioProcessor& proc;
     
