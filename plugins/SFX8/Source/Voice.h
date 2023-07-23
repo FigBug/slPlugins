b867cc8a120e260b2012d14da38d452e3e33a3aa
Roland Rabien <figbug@gmail.com>
2023-07-23 06:49:02 -0700

    Fix headers

--- plugins/SFX8/Source/Voice.h
+++ plugins/SFX8/Source/Voice.h
@@ -10,7 +10,7 @@
 
 #pragma once
 
-#include "../JuceLibraryCode/JuceHeader.h"
+#include <JuceHeader.h>
 
 #include "../../../modules/bfxr/PinkNumber.h"
 #include "../../../modules/bfxr/SfxrParams.h"
