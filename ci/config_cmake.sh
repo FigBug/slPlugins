eebe31651779bb405486579d5e70ee86e6d01983
Roland Rabien <figbug@gmail.com>
2025-11-22 16:13:20 -0800

    each plugin gets own cmake file

--- ci/config_cmake.sh
+++ ci/config_cmake.sh
@@ -5,12 +5,29 @@ cd "$ROOT"
 
 export PATH=$PATH:"/c/Program Files/CMake/bin"
 
+# Determine generator based on platform
 if [ "$(uname)" == "Darwin" ]; then
-  TOOLCHAIN="xcode"
+  GENERATOR="Xcode"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  TOOLCHAIN="ninja-gcc"
+  GENERATOR="Ninja"
+  export CMAKE_C_COMPILER=clang
+  export CMAKE_CXX_COMPILER=clang++
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-  TOOLCHAIN="vs"
+  GENERATOR="Visual Studio 17 2022"
 fi
 
-cmake --preset $TOOLCHAIN -D BUILD_EXTRAS=OFF -D JUCE_COPY_PLUGIN_AFTER_BUILD=ON
+# Configure each plugin individually
+cd "$ROOT/ci"
+cat pluginlist.txt | while read PLUGIN; do
+  PLUGIN=$(echo $PLUGIN|tr -d '\n\r ')
+
+  echo "Configuring $PLUGIN..."
+  cd "$ROOT/plugins/$PLUGIN"
+
+  cmake -B build -G "$GENERATOR" -D JUCE_COPY_PLUGIN_AFTER_BUILD=ON
+
+  echo "Configured $PLUGIN"
+done
+
+echo ""
+echo "All plugins configured!"
