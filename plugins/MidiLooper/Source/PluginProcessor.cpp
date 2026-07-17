3798bdfea53638e5b71d120c371bbf23446d1dd8
Roland Rabien <figbug@gmail.com>
2026-07-16 21:00:37 -0700

    MidiLooper: add crash reporting

--- plugins/MidiLooper/Source/PluginProcessor.cpp
+++ plugins/MidiLooper/Source/PluginProcessor.cpp
@@ -1,4 +1,5 @@
 #include "PluginProcessor.h"
+#include <mutex>
 #include "PluginEditor.h"
 
 static gin::ProcessorOptions createProcessorOptions()
@@ -7,9 +8,32 @@ static gin::ProcessorOptions createProcessorOptions()
         .withMidiLearn();
 }
 
+// If the shared CrashReporter is installed, launch it once per process (on the
+// first plugin instance) so it can scan and upload any crash from last session.
+static void launchCrashReporterOnce()
+{
+    static std::once_flag flag;
+    std::call_once (flag, []
+    {
+       #if JUCE_MAC
+        juce::File app ("/Library/Application Support/Rabien Software/Crash Reporter/CrashReporter.app");
+       #elif JUCE_WINDOWS
+        auto app = juce::File::getSpecialLocation (juce::File::globalApplicationsDirectory)
+                       .getChildFile ("Rabien Software").getChildFile ("Crash Reporter").getChildFile ("CrashReporter.exe");
+       #else
+        juce::File app;
+       #endif
+
+        if (app.exists())
+            juce::Process::openDocument (app.getFullPathName(), {});
+    });
+}
+
 MidiLooperAudioProcessor::MidiLooperAudioProcessor()
     : gin::Processor (BusesProperties(), false, createProcessorOptions())
 {
+    launchCrashReporterOnce();
+
     midiPlayer.setLooping (true);
     init();
 }
